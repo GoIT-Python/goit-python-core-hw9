8daa87e979991ea1c7315fb23194c3d326f409bd
Volodymyr Kurov <vokur13@gmail.com>
2023-05-13 17:58:34 +0300

    cli_parser modified

--- hw9/cli_parser.py
+++ hw9/cli_parser.py
@@ -1,23 +1,6 @@
 import re
 
 
-def input_error(func):
-    def wrapper(*args, **kwargs):
-        try:
-            result = func(*args, **kwargs)
-        except KeyError:
-            print(KeyError)
-        except ValueError:
-            print(ValueError)
-        except IndexError:
-            print(IndexError)
-        else:
-            return result
-
-    return wrapper
-
-
-# @input_error
 def cli_parser(commands):
     cli = []
     query = input().strip().casefold()
@@ -26,8 +9,6 @@ def cli_parser(commands):
         if res:
             cli.append(res.group())
             cli.extend(re.sub(command, "", query).split(" ")[1:])
-    # if not cli[0] in commands:
-    #     raise ValueError("Wrong command")
     return cli
 
 
